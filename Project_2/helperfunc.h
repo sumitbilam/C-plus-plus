637f8a5f14ddc649bca0a59b1208ad446f14a288
Sumit Bilam <32146718+sumitbilam@users.noreply.github.com>
2018-09-14 21:59:36 -0400

    Add files via upload

---
+++ Project_2/helperfunc.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void write_account();
+
+void display_sp(int n);
+
+void modify_account(int n);
+
+void delete_account(int n);
+
+void display_all();
+
+void deposit_withdraw(int n, int option);
+
+void intro();
